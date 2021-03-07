7d216a9d4023b63360136df59a394ea97ad44771
Harvey Nighy <harveynighy@talktalk.net>
2021-03-07 21:35:51 +0000

    Completed Password Generator
    I have completed the first function however some optimization is required.

--- password_list.py
+++ password_list.py
@@ -1,8 +1,14 @@
 import random
-charecters = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',1,2,3,4,5,6,7,8,9]
-
+characters = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',1,2,3,4,5,6,7,8,9]
+password = []
+password_list = []
 def password_generator():
-    pass
+    print(random.choice(characters))
+    while len(password) < 8:
+        password.append(random.choice(characters))
 
 def password_lister():
     pass
+
+password_generator()
+print(password)
