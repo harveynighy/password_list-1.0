22dc1c5ea9edb09d601d93e78ad8166b1a9fbc38
Harvey Nighy <harveynighy@talktalk.net>
2021-03-07 22:25:20 +0000

    Password lister
    Have completed the password lister function.

--- password_list.py
+++ password_list.py
@@ -1,14 +1,19 @@
 import random
 characters = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',1,2,3,4,5,6,7,8,9]
-password = []
-password_list = []
+
 def password_generator():
-    print(random.choice(characters))
-    while len(password) < 8:
+    password = []
+    while len(password) < 4:
         password.append(random.choice(characters))
+    return password
 
 def password_lister():
-    pass
+    password_list = []
+    while len(password_list) < 5:
+        password_list.append(password_generator())
+    password_list = str(password_list)
+    print(len(password_list))
+    return password_list
 
-password_generator()
-print(password)
+print(password_generator())
+print(password_lister())
