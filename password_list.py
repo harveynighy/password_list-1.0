294a239e69fbc1f28f40dcfa921d2c9acb200c55
Harvey Nighy <harveynighy@talktalk.net>
2021-03-07 22:51:38 +0000

    Correct version
    Finished Code

--- password_list.py
+++ password_list.py
@@ -3,17 +3,20 @@ characters = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q
 
 def password_generator():
     password = []
-    while len(password) < 4:
+    while len(password) < 8:
         password.append(random.choice(characters))
+    password = str(password)    
     return password
 
 def password_lister():
     password_list = []
-    while len(password_list) < 5:
+    while len(password_list) < 15:
         password_list.append(password_generator())
     password_list = str(password_list)
     return password_list
 
-f = open('passwordlist.txt', 'a')
+print(password_lister())
+
+f = open('passwordlist.txt', 'w')
 f.write(password_lister())
 f.close()
